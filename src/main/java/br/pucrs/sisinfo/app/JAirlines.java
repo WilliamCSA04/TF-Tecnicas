2a9da23ebe5e6b935075bddd458650802f643596
othman853 <anuar.yasser@gmail.com>
2016-05-30 22:38:08 -0300

    @othman853 - Testando identidade de conexões compartilhadas

--- src/main/java/br/pucrs/sisinfo/app/JAirlines.java
+++ src/main/java/br/pucrs/sisinfo/app/JAirlines.java
@@ -1,10 +1,30 @@
 package br.pucrs.sisinfo.app;
 
+import br.pucrs.sisinfo.config.guice.GuiceConfig;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import java.sql.Connection;
+
+
 public class JAirlines {
-
+    
+    public Connection connection;
+    
+    @Inject
+    public JAirlines(Connection connection) {
+        this.connection = connection;
+    }
+    
     public static void main(String[] args) {
-      
-
+        
+        Injector injector = Guice.createInjector(new GuiceConfig());
+        
+        JAirlines airlines = injector.getInstance(JAirlines.class);
+        JAirlines airlines2 = injector.getInstance(JAirlines.class);
+        
+        System.out.println(airlines.connection == airlines2.connection);
+        
     }
 
 }
