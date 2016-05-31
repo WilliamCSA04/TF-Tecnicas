c80045bcb14de0604857d10724096ca5d08c6045
othman853 <anuar.yasser@gmail.com>
2016-05-30 22:37:33 -0300

    @othman853 - binding de Connection com provider em escopo Singleton

--- src/main/java/br/pucrs/sisinfo/config/guice/GuiceConfig.java
+++ src/main/java/br/pucrs/sisinfo/config/guice/GuiceConfig.java
@@ -1,16 +1,19 @@
-package br.pucrs.sisinfo.app;
+package br.pucrs.sisinfo.config.guice;
 
 import br.pucrs.sisinfo.interpretadores.Interpretador;
 import br.pucrs.sisinfo.interpretadores.InterpretadorDeDataBrasileira;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
+import java.sql.Connection;
 import java.util.GregorianCalendar;
 import java.util.Optional;
+import br.pucrs.sisinfo.config.providers.ConnectionProvider;
+import com.google.inject.Scopes;
 
 
 public class GuiceConfig extends AbstractModule{
-
+   
     @Override
     protected void configure() {
         
@@ -18,6 +21,10 @@ public class GuiceConfig extends AbstractModule{
                 .annotatedWith(Names.named("interpretadorData"))
                 .to(InterpretadorDeDataBrasileira.class);
         
+        bind(Connection.class)
+                .toProvider(ConnectionProvider.class)
+                .in(Scopes.SINGLETON);
+                
     }
     
    
