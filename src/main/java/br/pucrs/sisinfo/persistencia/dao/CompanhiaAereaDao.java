0c7a0ca8bf30f413fc0d8290530bbccfc5f43474
williamCSA04 <williamcaetano14@gmail.com>
2016-06-18 16:32:10 -0300

    Deletando companhia aerea do dao

--- src/main/java/br/pucrs/sisinfo/persistencia/dao/CompanhiaAereaDao.java
+++
@@ -1,7 +0,0 @@
-package br.pucrs.sisinfo.persistencia.dao;
-
-import br.pucrs.sisinfo.persistencia.modelo.CompanhiaAerea;
-
-public interface CompanhiaAereaDao extends Inserter<CompanhiaAerea>{
-    
-}
