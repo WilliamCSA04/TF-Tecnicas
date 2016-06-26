d43ffbc40607feef5c4c6c27406c37c5e058bc66
JaderTS <jader-fernandes@live.com>
2016-06-26 03:15:24 -0300

    classes de teste restauradas

--- src/test/java/br/pucrs/sisinfo/controllerTest/PesquisaControllerTest.java
+++ src/test/java/br/pucrs/sisinfo/controllerTest/PesquisaControllerTest.java
@@ -5,15 +5,24 @@
  */
 package br.pucrs.sisinfo.controllerTest;
 
+import br.pucrs.sisinfo.negocio.controller.PesquisaController;
+import br.pucrs.sisinfo.negocio.services.interpretadores.Interpretador;
+import java.util.GregorianCalendar;
+import java.util.Optional;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
  * @author Jader Cunha
  */
 public class PesquisaControllerTest {
-    
+    private PesquisaController pesquisa;
+    private Interpretador<Optional<GregorianCalendar>> interpretador;
         
-    
+    @BeforeClass
+    public static void setUpClass() {
+        PesquisaController pesquisa = new PesquisaController();
+    }    
 }
