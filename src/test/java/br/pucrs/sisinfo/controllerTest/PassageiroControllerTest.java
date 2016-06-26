e7d0b377fa9b58e22537220937e3fe0dccaae0e4
JaderTS <jader-fernandes@live.com>
2016-06-26 03:02:16 -0300

    code

--- src/test/java/br/pucrs/sisinfo/controllerTest/PassageiroControllerTest.java
+++ src/test/java/br/pucrs/sisinfo/controllerTest/PassageiroControllerTest.java
@@ -5,16 +5,39 @@
  */
 package br.pucrs.sisinfo.controllerTest;
 
+import br.pucrs.sisinfo.negocio.controller.PassageiroController;
+import br.pucrs.sisinfo.persistencia.modelo.Passageiro;
+import java.util.GregorianCalendar;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Jader Cunha
+ * @author Dell Inc
  */
 public class PassageiroControllerTest {
     
+    private PassageiroController _passageiroController;
+    
     public PassageiroControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUp() {
+        PassageiroController _passageiroController = new PassageiroController();
+    }
+    
+    @Test
+    public void buscarUsuario(){
+        String _email = "mel@caomenor.com";
+        String _senha = "melzinha";
+        
+        int _test = _passageiroController.buscaUsuario(_email, _senha);
+        assertTrue("O usuário deve ser o número 1 = ",_passageiroController.equals(_test));
     }
     
 }
