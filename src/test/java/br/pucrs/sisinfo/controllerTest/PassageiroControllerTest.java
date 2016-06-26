2883a218eea495a49ce2e47d684d7ac94443b7cd
JaderTS <williamcaetano14@gmail.com>
2016-06-26 01:55:01 -0300

    refatorando code de teste

---
+++ src/test/java/br/pucrs/sisinfo/controllerTest/PassageiroControllerTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.pucrs.sisinfo.controllerTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jader Cunha
+ */
+public class PassageiroControllerTest {
+    
+    public PassageiroControllerTest() {
+    }
+    
+}
