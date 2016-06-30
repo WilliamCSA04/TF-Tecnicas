5123ec9ff147bc58eb28a46ffb645da3a3665e33
williamcsa04 <williamcaetano14@gmail.com>
2016-06-30 05:22:16 -0300

    @WilliamCSA04 - Script de inserção de dados para mapa_assentos foi criado

--- src/main/java/resources/Insert_Voos.sql
+++ src/main/java/resources/Insert_Voos.sql
@@ -3,3 +3,9 @@ insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '
 insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '20:00:00', 3, '02/07/2016');
 insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '20:00:00', 1, '29/06/2016');
 insert into voos (partida, chegada, id_rota, data_partida) values ('08:00:00', '20:00:00', 2429, '15/07/2016');
+
+update voos set id_mapa_assentos=5 where id=5;
+update voos set id_mapa_assentos=4 where id=4;
+update voos set id_mapa_assentos=3 where id=3;
+update voos set id_mapa_assentos=2 where id=2;
+update voos set id_mapa_assentos=1 where id=1;
---
+++ src/main/java/resources/Insert_mapa_assentos.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  William
+ * Created: 30/06/2016
+ */
+
