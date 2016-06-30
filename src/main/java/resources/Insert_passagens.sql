f09a93a3238e53d4d4a994d2f9ffdf8ebfaa39db
williamcsa04 <williamcaetano14@gmail.com>
2016-06-30 05:10:15 -0300

    @WilliamCSA04 - Alteração na tabela de voos

--- src/main/java/resources/Insert_passagens.sql
+++ src/main/java/resources/Insert_passagens.sql
@@ -2,4 +2,4 @@ insert into passagens (id, voo_id, passageiro_id, status) values(1, 3, 100, 'Nao
 insert into passagens (id, voo_id, passageiro_id, status) values(2, 5, 100, 'Utilizada');
 insert into passagens (id, voo_id, passageiro_id, status) values(3, 2, 100, 'Check-in ok');
 insert into passagens (id, voo_id, passageiro_id, status) values(4, 4, 100, 'Check-in aberto');
-insert into passagens (id, voo_id, passageiro_id, status) values(5, 6, 100, 'Pendente');
+insert into passagens (id, voo_id, passageiro_id, status) values(5, 1, 100, 'Pendente');
--- src/main/java/resources/create_database.sql
+++ src/main/java/resources/create_database.sql
@@ -17,15 +17,33 @@ CREATE TABLE IF NOT EXISTS rotas(
 
 );
 
+CREATE TABLE IF NOT EXISTS mapa_assentos (
+    id SERIAL,
+    poltrona1 BOOLEAN,
+    poltrona2 BOOLEAN,
+    poltrona3 BOOLEAN,
+    poltrona4 BOOLEAN,
+    poltrona5 BOOLEAN,
+    poltrona6 BOOLEAN,
+    poltrona7 BOOLEAN,
+    poltrona8 BOOLEAN,
+    poltrona9 BOOLEAN,
+    poltrona10 BOOLEAN,
+
+    CONSTRAINT PK_MAPA_ASSENTOS PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS voos (
     id SERIAL,
     partida TIME,
     chegada TIME,
     id_rota INTEGER,
+    id_mapa_assentos INTEGER,
     data_partida DATE,
 
     CONSTRAINT PK_VOOS PRIMARY KEY (id),
-    CONSTRAINT FK_VOOS_ROTAS FOREIGN KEY (id_rota) REFERENCES rotas (id)
+    CONSTRAINT FK_VOOS_ROTAS FOREIGN KEY (id_rota) REFERENCES rotas (id),
+    CONSTRAINT FK_MAPA_ASSENTOS FOREIGN KEY (id_mapa_assentos) REFERENCES mapa_assentos(id)
 );
 
 
