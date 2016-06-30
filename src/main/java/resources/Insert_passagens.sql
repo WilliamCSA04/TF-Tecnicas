c4ac0230215c5f4a6425849f38effeccade92b78
williamcsa04 <williamcaetano14@gmail.com>
2016-06-29 23:10:57 -0300

    @WilliamCSA04 - Feito arquivo com sql para inserir passagens no banco

--- src/main/java/resources/Insert_passagens.sql
+++ src/main/java/resources/Insert_passagens.sql
@@ -1,5 +1,5 @@
--- insert into passagens (id, voo_id, passageiro_id, status) values(1, 2, 28/06/2016, 100, "Nao Usada");
--- insert into passagens (id, voo_id, passageiro_id, status) values(2, 1, 29/06/2016, 100, "Utilizada");
--- insert into passagens (id, voo_id, passageiro_id, status) values(3, 15, 30/06/2016, 100, "Check-in ok");
--- insert into passagens (id, voo_id, passageiro_id, status) values(4, 3, 02/07/2016, 100, "Check-in aberto");
--- insert into passagens (id, voo_id, passageiro_id, status) values(5, 2429, 15/07/2016, 100, "Pendente");
+insert into passagens (id, voo_id, passageiro_id, status) values(1, 3, 100, 'Nao Usada');
+insert into passagens (id, voo_id, passageiro_id, status) values(2, 5, 100, 'Utilizada');
+insert into passagens (id, voo_id, passageiro_id, status) values(3, 2, 100, 'Check-in ok');
+insert into passagens (id, voo_id, passageiro_id, status) values(4, 4, 100, 'Check-in aberto');
+insert into passagens (id, voo_id, passageiro_id, status) values(5, 6, 100, 'Pendente');
