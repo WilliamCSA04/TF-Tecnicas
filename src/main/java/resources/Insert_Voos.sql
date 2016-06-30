913b2643ac233740b15c2af0f3e869786a394d06
williamcsa04 <williamcaetano14@gmail.com>
2016-06-29 22:40:55 -0300

    @WilliamCSA04 - Adicinado o sql de insert dos voos

---
+++ src/main/java/resources/Insert_Voos.sql
@@ -0,0 +1,5 @@
+insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '20:00:00', 15, '30/06/2016');
+insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '20:00:00', 2, '28/06/2016');
+insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '20:00:00', 3, '02/07/2016');
+insert into voos (partida, chegada, id_rota, data_partida) values ('10:00:00', '20:00:00', 1, '29/06/2016');
+insert into voos (partida, chegada, id_rota, data_partida) values ('08:00:00', '20:00:00', 2429, '15/07/2016');
